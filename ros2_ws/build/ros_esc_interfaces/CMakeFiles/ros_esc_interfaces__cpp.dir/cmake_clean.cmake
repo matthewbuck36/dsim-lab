5ebe8b8352a1161b7e66e51bcee9a059f3c12d40
matthewbuck36 <71412328+matthewbuck36@users.noreply.github.com>
2025-12-04 14:26:22 -0800

    Add files via upload

---
+++ ros2_ws/build/ros_esc_interfaces/CMakeFiles/ros_esc_interfaces__cpp.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_esc_interfaces__cpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64__builder.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64__struct.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64__traits.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64__type_support.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64_multi_array__builder.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64_multi_array__struct.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64_multi_array__traits.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_float64_multi_array__type_support.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_string__builder.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_string__struct.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_string__traits.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_string__type_support.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_transform_multi_array__builder.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_transform_multi_array__struct.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_transform_multi_array__traits.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/stamped_transform_multi_array__type_support.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/timekeeper__builder.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/timekeeper__struct.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/timekeeper__traits.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/detail/timekeeper__type_support.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/stamped_float64.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/stamped_float64_multi_array.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/stamped_string.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/stamped_transform_multi_array.hpp"
+  "rosidl_generator_cpp/ros_esc_interfaces/msg/timekeeper.hpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ros_esc_interfaces__cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
